f1e2c58d2882143e319f0aee19d9d635c8c511f7
Samurai <kylett63@gmail.com>
2023-04-26 14:36:54 +0200

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat myCat = new Cat();
+            myCat.Name = "Fluffy";
+            myCat.Age = 5;
+            
+            Console.WriteLine("Namn" + myCat.Name);
+            Console.WriteLine("år" + myCat.Age);
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
